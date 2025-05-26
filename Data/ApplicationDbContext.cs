72482305ca31ac8a0a399a26aa45497cc9f4a9ce
P.T Nyamuda <ptnrlab@gmail.com>
2025-05-26 13:11:25 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Frontfolio.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext:DbContext
+    {
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+

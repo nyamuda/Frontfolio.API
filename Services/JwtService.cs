fbbf51ac99090143f7661c4402437feca834faa3
P.T Nyamuda <ptnrlab@gmail.com>
2025-05-28 10:45:27 +0200

    jwt settings fetched and signing credentials created in jwt service constructor

--- Services/JwtService.cs
+++ Services/JwtService.cs
@@ -7,30 +7,31 @@ using System.Text;
 public class JwtService
 {
     private readonly ApplicationDbContext _context;
-    private readonly IConfiguration _config;
+    private string _jwtIssuer = string.Empty;
+    private string _jwtAudience =string.Empty ;
+    private string _jwtKey =string.Empty;
+    private SigningCredentials _signingCredentials;
+
     public JwtService(ApplicationDbContext context, IConfiguration config)
     {
         _context = context;
-        _config = config;
+       
+        //get the JWT settings
+        _jwtIssuer = config.GetValue<string>("Authentication:Jwt:Issuer") ?? throw new KeyNotFoundException("Jwt issuer field not found.");
+        _jwtAudience = config.GetValue<string>("Authentication:Jwt:Audience") ?? throw new KeyNotFoundException("Jwt audience field not found.");
+        _jwtKey = config.GetValue<string>("Authentication:Jwt:Key") ?? throw new KeyNotFoundException("Jwt key field not found.");
+
+        //encode the key and create the signing credentials
+        var encodedJwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+        _signingCredentials = new SigningCredentials(encodedJwtKey, SecurityAlgorithms.HmacSha256);
     }
 
 
-    public async string GenerateJwtToken(int userId, double expirationInMinutes=10)
+    public async Task<string> GenerateJwtToken(int userId, double expirationInMinutes=10)
     {
         //get the details of the user the token is for
         var user = await  _context.Users.FirstOrDefaultAsync(u => u.Id.Equals(userId));
         if (user is null) throw new KeyNotFoundException($"User with ID {userId} does not exist.");
-
-        //get the JWT settings
-        string jwtIssuer = _config.GetValue<string>("Authentication:Jwt:Issuer") ?? throw new KeyNotFoundException("Jwt issuer field not found.");
-        string jwtAudience = _config.GetValue<string>("Authentication:Jwt:Audience") ?? throw new KeyNotFoundException("Jwt audience field not found.");
-        string jwtKey = _config.GetValue<string>("Authentication:Jwt:Key") ?? throw new KeyNotFoundException("Jwt key field not found.");
-
-        //encode the key and create the JWT signature
-        var encodedJwtKey =new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
-        var credentials = new SigningCredentials(encodedJwtKey, SecurityAlgorithms.HmacSha256);
-
-
 
         //Create claims for the token
         // Claims represent data stored in the token
@@ -43,15 +44,20 @@ public class JwtService
 
         //finally create the token
         var token = new JwtSecurityToken(
-            issuer: jwtIssuer,
-            audience: jwtAudience,
-            signingCredentials: credentials,
+            issuer: _jwtIssuer,
+            audience: _jwtAudience,
+            signingCredentials: _signingCredentials,
             claims: claims,
             expires: DateTime.Now.AddMinutes(expirationInMinutes)
             );
 
 
         return new JwtSecurityTokenHandler().WriteToken(token);
+
+    }
+
+
+    public ClaimsPrincipal ValidateJwtToken(string token) { 
 
     }
 }

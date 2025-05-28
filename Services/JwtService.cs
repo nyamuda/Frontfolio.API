477d11ffb2a5ffaaf4b5371ffc2db7c058d79c41
P.T Nyamuda <ptnrlab@gmail.com>
2025-05-28 11:02:08 +0200

    validate Jwt token service method add

--- Services/JwtService.cs
+++ Services/JwtService.cs
@@ -1,4 +1,5 @@
 ﻿using Frontfolio.API.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -9,8 +10,8 @@ public class JwtService
     private readonly ApplicationDbContext _context;
     private string _jwtIssuer = string.Empty;
     private string _jwtAudience =string.Empty ;
-    private string _jwtKey =string.Empty;
-    private SigningCredentials _signingCredentials;
+    private SymmetricSecurityKey _securityKey;
+  
 
     public JwtService(ApplicationDbContext context, IConfiguration config)
     {
@@ -19,11 +20,11 @@ public class JwtService
         //get the JWT settings
         _jwtIssuer = config.GetValue<string>("Authentication:Jwt:Issuer") ?? throw new KeyNotFoundException("Jwt issuer field not found.");
         _jwtAudience = config.GetValue<string>("Authentication:Jwt:Audience") ?? throw new KeyNotFoundException("Jwt audience field not found.");
-        _jwtKey = config.GetValue<string>("Authentication:Jwt:Key") ?? throw new KeyNotFoundException("Jwt key field not found.");
+        var jwtKey = config.GetValue<string>("Authentication:Jwt:Key") ?? throw new KeyNotFoundException("Jwt key field not found.");
 
-        //encode the key and create the signing credentials
-        var encodedJwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
-        _signingCredentials = new SigningCredentials(encodedJwtKey, SecurityAlgorithms.HmacSha256);
+        //encode the key
+        _securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+       
     }
 
 
@@ -32,6 +33,9 @@ public class JwtService
         //get the details of the user the token is for
         var user = await  _context.Users.FirstOrDefaultAsync(u => u.Id.Equals(userId));
         if (user is null) throw new KeyNotFoundException($"User with ID {userId} does not exist.");
+
+        //create the signing credentials
+        SigningCredentials _signingCredentials = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha256);
 
         //Create claims for the token
         // Claims represent data stored in the token
@@ -57,7 +61,26 @@ public class JwtService
     }
 
 
-    public ClaimsPrincipal ValidateJwtToken(string token) { 
+    //Manually validate a Jwt token
+    public ClaimsPrincipal ValidateJwtToken(string token) {
+
+        //parameters used to validate the token
+        var validationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _jwtIssuer,
+            ValidAudience = _jwtAudience,
+            IssuerSigningKey = _securityKey
+        };
+
+        //validate the provided Jwt token
+        ClaimsPrincipal claimsPrincipal = new JwtSecurityTokenHandler()
+            .ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+
+        return claimsPrincipal;
 
     }
 }

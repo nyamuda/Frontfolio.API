9dbd74a77303c4a4724d7515378366e65c4a642f
P.T Nyamuda <ptnrlab@gmail.com>
2025-05-28 11:16:47 +0200

    Add user dto created

--- Dtos/AddUserDto.cs
+++ Dtos/AddUserDto.cs
@@ -2,5 +2,8 @@
 {
     public class AddUserDto
     {
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } =default!;
+        public string Password { get; set; } = default!;
     }
 }

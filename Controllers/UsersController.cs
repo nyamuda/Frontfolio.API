e4ff4dcfaf052246a9a300fa15b633ff186023bd
P.T Nyamuda <ptnrlab@gmail.com>
2025-05-29 21:08:46 +0200

    Added access control to get user by ID controller method: users can only access their own data based on token ID

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Frontfolio.API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 
 [Route("api/[controller]")]
@@ -8,20 +10,34 @@ using Microsoft.AspNetCore.Mvc;
 public class UsersController : ControllerBase
 {
     private readonly UserService _userService;
+    private readonly JwtService _jwtService;
 
 
-    public UsersController(UserService userService)
+    public UsersController(UserService userService, JwtService jwtService)
     {
         _userService = userService;
+        _jwtService = jwtService;
     }
 
     //Get user by ID
     [HttpGet("{id}")]
+    [Authorize]
     public async Task<IActionResult> Get(int id)
     {
         try
         {
+            //get the JWT
+            var accessToken = HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            //validate the token 
+            ClaimsPrincipal claimsPrincipal = _jwtService.ValidateJwtToken(accessToken);
+            //The ID of the user from the token
+            string tokenUserId = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new KeyNotFoundException("Access token lacks the name identifier claim.");
+
+            //For a user to access this resource
+            //the ID from their access token must match the ID of the user they're trying to access
             var user = await _userService.GetUserById(id);
+            if (!user.Id.ToString().Equals(tokenUserId))
+                return Forbid(ErrorMessageHelper.ForbiddenErrorMessage());
 
             return Ok(user);
 
--- Helpers/ErrorMessageHelper.cs
+++ Helpers/ErrorMessageHelper.cs
@@ -5,5 +5,8 @@ public static class ErrorMessageHelper
     public static string UnexpectedErrorMessage() =>
         "The server encountered an unexpected issue. Please try again later.";
 
+    public static string ForbiddenErrorMessage() =>
+        "You do not have permission to access this resource.";
+
 
 }

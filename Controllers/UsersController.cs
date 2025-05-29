7b0b5ceb0ac3a2f50a732bf760dcd05e7d14df97
P.T Nyamuda <ptnrlab@gmail.com>
2025-05-29 20:21:22 +0200

    users controller added

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Frontfolio.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        //Get user by ID
+        public async
+    }
+}
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,16 +1,37 @@
 ﻿using Frontfolio.API.Data;
+using Frontfolio.API.Dtos.Auth;
+using Microsoft.EntityFrameworkCore;
 
 namespace Frontfolio.API.Services
 {
     public class UserService
     {
 
-        public readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
 
         public UserService(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        
+        public async Task<UserDto> GetUserById(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id.Equals(id));
+
+            if (user == null) throw new KeyNotFoundException($@"User with ID ""{id}"" does not exist");
+
+            return UserDto.MapFrom(user);
+        }
+
+        public async Task<string> GenerateUserOtp(string email)
+        {
+            //first, get the user(
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email.Equals(email));
+            if (user is null) throw new KeyNotFoundException($@"User with email ""{email}"" does not exist.");
+
+
         }
     }
 }

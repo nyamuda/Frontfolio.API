0c11d44eb4748a17ab213634b5a0f558b94b5d2f
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-03 08:53:02 +0200

    resetToken property added to reset password dto

--- Dtos/Auth/ResetPasswordDto.cs
+++ Dtos/Auth/ResetPasswordDto.cs
@@ -2,7 +2,9 @@
 using System.ComponentModel.DataAnnotations;
 
 public class ResetPasswordDto
-    {
+{
+    [Required]
+    public string ResetToken { get; set; } = default!;
     [Required]
     [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$",
        ErrorMessage = "Password must be at least 8 characters long and contain a mix of letters, numbers, and special characters")]

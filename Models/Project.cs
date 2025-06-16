4b702e4f41b0f7d8eaab7cb07a8da325bb03b345
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-16 22:48:03 +0200

    Project model properties added

--- Models/Project.cs
+++ Models/Project.cs
@@ -2,6 +2,20 @@
 {
     public class Project
     {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Summary { get; set; }
+
+        public List<string> TechStack { get; set; } = [];
+
+        public required string GitHubUrl { get; set; }
+
+        public required string ImageUrl { get; set; }
+
+        public required string LiveUrl { get; set; }
+
 
     }
 }

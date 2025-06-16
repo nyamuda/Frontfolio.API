3344c44a7758b40109b4cf0c7cc3d15c494a78b6
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-16 22:54:48 +0200

    project id added t paragraph as foreign key

--- Models/Paragraph.cs
+++ Models/Paragraph.cs
@@ -7,5 +7,7 @@
         public required string Content { get; set; }
         public string ImageUrl { get; set; } = default!;
         public string ImageCaption { get; set; } = default!;
+        public int ProjectId { get; set; }  
+        public Project? Project { get; set; }
     }
 }

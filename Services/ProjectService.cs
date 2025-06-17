7b1ac96895595912fd90201eed9d83aa079029d9
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-17 07:56:48 +0200

    project service created and context injected

---
+++ Services/ProjectService.cs
@@ -0,0 +1,13 @@
+﻿
+using Frontfolio.API.Data;
+
+public class ProjectService
+    {
+
+    private readonly ApplicationDbContext _context;
+
+    public ProjectService(ApplicationDbContext context) { 
+        _context = context;
+    }
+    }
+

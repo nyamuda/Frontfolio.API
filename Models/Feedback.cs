83c9c5e45119ead0416981497ba20cd1de29f1ce
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-19 08:42:55 +0200

    Feedback model created

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,6 +1,23 @@
-﻿namespace Frontfolio.API.Models
+﻿
+using Frontfolio.API.Models;
+
+public class Feedback
 {
-    public class Feedback
-    {
-    }
+    public int Id { get; set; }
+
+    public required string AuthorName { get; set; }
+
+    public string? AuthorRole { get; set; }
+
+    public required string Comment { get; set; }
+
+    public required int ProjectId { get; set; }
+
+    public Project? Project { get; set; }
+
+    public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? CreatedAt { get; set; }
+
 }
+

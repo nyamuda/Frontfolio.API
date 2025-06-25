8e16e90ad1f9b962a81cb7f2430687211eec2882
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-25 05:31:56 +0200

    ProjectSortOption enum created

---
+++ Enums/ProjectSortOption.cs
@@ -0,0 +1,7 @@
+﻿public enum ProjectSortOption
+{
+    StartDate,
+    EndDate,
+    OrderLevel
+
+}
--- Models/Project.cs
+++ Models/Project.cs
@@ -6,6 +6,9 @@
 
         public required string Title { get; set; }
 
+
+        public int OrderLevel { get; set; } 
+
         public DateTime StartDate { get; set; } = DateTime.UtcNow;
 
         public DateTime EndDate { get; set; } = DateTime.UtcNow;

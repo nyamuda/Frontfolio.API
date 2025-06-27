6cdf33108b8cb1899a410d89e87cadd9a3b72ed7
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-27 10:47:01 +0200

    created controller method to update project background paragraph

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -285,7 +285,7 @@ public class ProjectsController : ControllerBase
     }
     //Add a background paragraph for a project with a given ID
     [HttpPost("{projectId}/backgrounds")]
-    public async Task<IActionResult> AddParagraph(int projectId,AddParagraphDto paragraphDto)
+    public async Task<IActionResult> AddBackgroundParagraph(int projectId,AddParagraphDto paragraphDto)
     {
         try
         {
@@ -299,8 +299,10 @@ public class ProjectsController : ControllerBase
 
             if(int.TryParse(tokenUserId, out int userId))
             {
-                ParagraphDto paragraph = await _paragraphService
+                await _paragraphService
                     .AddProjectBackgroundParagraph(projectId,userId, paragraphDto);
+
+                return StatusCode(201);
             }
             //throw an exception if tokenUserId cannot be parsed
             throw new UnauthorizedAccessException("Access denied. Token does not contain a valid user ID claim.");
@@ -327,5 +329,56 @@ public class ProjectsController : ControllerBase
             return StatusCode(500, response);
         }
     }
+
+    //Update background paragraph for a specific project
+    [HttpPut("{projectId}/backgrounds/{backgroundId}")]
+    public async Task<IActionResult> UpdateBackgroundParagraph(int projectId,int backgroundId,UpdateParagraphDto paragraphDto)
+    {
+        try
+        {
+            //Retrieve the access token from the request
+            var token = HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            //Manually validate the token
+            ClaimsPrincipal claims = _jwtService.ValidateJwtToken(token);
+            //Get the user ID claim from the token
+            string tokenUserId = claims.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? throw new UnauthorizedAccessException("Access denied. Token does not contain a valid user ID claim.");
+
+            if (int.TryParse(tokenUserId, out int userId))
+            {
+                await _paragraphService
+                    .UpdateProjectBackgroundParagraph(projectId, backgroundId, userId, paragraphDto);
+
+                return NoContent();
+            }
+            //throw an exception if tokenUserId cannot be parsed
+            throw new UnauthorizedAccessException("Access denied. Token does not contain a valid user ID claim.");
+
+        }
+
+         catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { message = ex.Message });
+
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            return Unauthorized(new { message = ex.Message });
+
+        }
+        catch (Exception ex)
+        {
+            var response = new
+            {
+                message = ErrorMessageHelper.UnexpectedErrorMessage(),
+                details = ex.Message
+            };
+
+            return StatusCode(500, response);
+        }
+
+    }
+
+
     
 }

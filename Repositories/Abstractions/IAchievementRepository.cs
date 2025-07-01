f72137a870966ab6dfe84c9d3340a990069882ae
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-01 20:15:27 +0200

    created an abstraction for the achievement repository

--- Repositories/Abstractions/IAchievementRepository.cs
+++ Repositories/Abstractions/IAchievementRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Frontfolio.API.Repositories.Abstractions
+﻿using Frontfolio.API.Models;
+
+namespace Frontfolio.API.Repositories.Abstractions
 {
     public interface IAchievementRepository
     {
+        Task<ParagraphDto> DeleteByIdAsync(int id, int projectId);
+
+        Task AddIfNotExistingAsync(int projectId, List<Achievement> incomingAchievements);
+
+        Task UpdateExistingAsync(int projectId, List<Achievement> incomingAchievements);
     }
 }

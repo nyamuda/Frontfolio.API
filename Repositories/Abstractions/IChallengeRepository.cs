101c6acfb2716afd2e2b13b6d4aef98df68dcf1e
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-01 20:11:48 +0200

    created an abstraction for the challenge repository

--- Repositories/Abstractions/IChallengeRepository.cs
+++ Repositories/Abstractions/IChallengeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Frontfolio.API.Repositories.Abstractions
+﻿using Frontfolio.API.Models;
+
+namespace Frontfolio.API.Repositories.Abstractions
 {
     public interface IChallengeRepository
     {
+        Task<ParagraphDto> DeleteByIdAsync(int id, int projectId);
+
+        Task AddIfNotExistingAsync(int projectId, List<Challenge> incomingChallenges);
+
+        Task UpdateExistingAsync(int projectId, List<Challenge> incomingChallenges);
     }
 }

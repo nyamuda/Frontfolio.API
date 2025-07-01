0797af45b8876af2dbab072c3e49c8d852f7911c
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-01 20:13:52 +0200

    created an abstraction for the feedback repository

--- Repositories/Abstractions/IFeedbackRepository.cs
+++ Repositories/Abstractions/IFeedbackRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Frontfolio.API.Repositories.Abstractions
+﻿using Frontfolio.API.Models;
+
+namespace Frontfolio.API.Repositories.Abstractions
 {
     public interface IFeedbackRepository
     {
+        Task<ParagraphDto> DeleteByIdAsync(int id, int projectId);
+
+        Task AddIfNotExistingAsync(int projectId, List<Feedback> incomingFeedback);
+
+        Task UpdateExistingAsync(int projectId, List<Feedback> incomingFeedback);
     }
 }

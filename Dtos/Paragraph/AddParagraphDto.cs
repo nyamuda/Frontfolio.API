d83abcee569c55dc63c1406868d0a3ecb8251bf2
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-02 13:31:14 +0200

    The `AddParagraphDto` class has been updated with a new required property `ParagraphType` of type `ParagraphType`, marked with the `[Required]` attribute

--- Dtos/Paragraph/AddParagraphDto.cs
+++ Dtos/Paragraph/AddParagraphDto.cs
@@ -7,10 +7,27 @@ public class AddParagraphDto
     public required string Title { get; set; }
     [Required]
     public required string Content { get; set; }
+
+    [Required]
+    public required ParagraphType ParagraphType { get; set; }
+
     [Url]
     public string? ImageUrl { get; set; } = default!;
 
     public string? ImageCaption { get; set; } = default!;
-    
+
+    public static Paragraph MapTo(AddParagraphDto paragraphDto)
+    {
+        return new Paragraph
+        {         
+            Title = paragraphDto.Title,
+            Content = paragraphDto.Content,
+            ImageUrl = paragraphDto.ImageUrl,
+            ImageCaption = paragraphDto.ImageCaption,
+            ParagraphType = paragraphDto.ParagraphType,          
+        };
+
+    }
+
 
 }

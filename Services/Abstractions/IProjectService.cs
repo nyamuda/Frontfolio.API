57b973fe80dba5fc7f0f95b463d069bbe62358fd
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-02 19:37:37 +0200

    Created an abstraction for the `ProjectService`

--- Services/Abstractions/IProjectService.cs
+++ Services/Abstractions/IProjectService.cs
@@ -2,5 +2,17 @@
 {
     public interface IProjectService
     {
+        Task<ProjectDto> Get(int id);
+
+        Task<List<ProjectDto>> Get();
+
+        Task<ProjectDto> Create(AddProjectDto projectDto);
+
+        Task Delete(int id);
+
+        Task Update(int id, UpdateProjectDto projectDto);
+
+
+
     }
 }

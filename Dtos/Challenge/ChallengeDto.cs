7b5b107781ac3e13441b64422a3370f6e295bf11
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-02 12:49:49 +0200

    Introduced new DTO classes for challenges: `AddChallengeDto`, `ChallengeDto`, and `UpdateChallengeDto`. `ChallengeDto` includes properties and a mapping method for converting `Challenge` entities to DTOs.

---
+++ Dtos/Challenge/ChallengeDto.cs
@@ -0,0 +1,32 @@
+﻿
+public class ChallengeDto
+    {
+    public required int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public required string Problem { get; set; }
+
+    public required string Solution { get; set; }
+
+    public required int ProjectId { get; set; }
+
+    public DateTime CreatedAt { get; set; } 
+
+    
+
+    public static ChallengeDto MapFrom(Challenge challenge)
+    {
+        return new ChallengeDto
+        {
+            Id = challenge.Id,
+            Title = challenge.Title,
+            Problem = challenge.Problem,
+            Solution = challenge.Solution,
+            ProjectId = challenge.ProjectId,
+            CreatedAt = challenge.CreatedAt
+
+        };
+    }
+}
+

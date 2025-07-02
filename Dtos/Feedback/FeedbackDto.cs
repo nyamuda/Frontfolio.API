9e6d721076b3e848ad01286c5ac242aec6143feb
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-02 12:32:03 +0200

    Added required modifiers and mapping method to FeedbackDto

--- Dtos/Feedback/FeedbackDto.cs
+++ Dtos/Feedback/FeedbackDto.cs
@@ -3,7 +3,7 @@ using Frontfolio.API.Models;
 
 public class FeedbackDto
 {
-    public int Id { get; set; }
+    public required int Id { get; set; }
 
     public required string AuthorName { get; set; }
 
@@ -13,8 +13,22 @@ public class FeedbackDto
 
     public required int ProjectId { get; set; }
 
-    public DateTime SubmittedAt { get; set; } 
+    public required DateTime SubmittedAt { get; set; } 
 
     public DateTime? CreatedAt { get; set; }
+
+
+    public static FeedbackDto MapFrom(Feedback feedback)
+    {
+        return new FeedbackDto
+        {
+            Id = feedback.Id,
+            AuthorName = feedback.AuthorName,
+            AuthorRole = feedback.AuthorRole,
+            Comment = feedback.Comment,
+            ProjectId = feedback.ProjectId,
+            SubmittedAt = feedback.SubmittedAt
+        };
+    }
 }
 

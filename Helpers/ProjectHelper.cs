3d52dd33b343dd5dcc226a71ff751f16a53871ca
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-03 07:16:17 +0200

    Created a customized error message in `EnsureUserOwnsProject` help based on the crud context

--- Helpers/ProjectHelper.cs
+++ Helpers/ProjectHelper.cs
@@ -9,11 +9,20 @@ public class ProjectHelper
     /// </summary>
     /// <param name="tokenUserId">The user ID extracted from the authenticated token.</param>
     /// <param name="project">The project to verify ownership of.</param>
+    /// <param name="crudContext">The context of the operation. Used to customize the error message</param>
     /// <exception cref="UnauthorizedAccessException">
     /// Thrown if the project does not belong to the specified user.
     /// </exception>
-    public static void EnsureUserOwnsProject(int tokenUserId, Project project,string errorMessage)
+    public static void EnsureUserOwnsProject(int tokenUserId, Project project,CrudContext crudContext)
     {
+        string errorMessage = crudContext switch
+        {
+            CrudContext.Create => "You don't have the permission to create this resource",
+            CrudContext.Update => "You don't have the permission to update this resource",
+            CrudContext.Delete => "You don't have the permission to delete this resource",
+            _ => "You don't have the permission to access this resource"
+
+        };
         if (project.UserId != tokenUserId)
             throw new UnauthorizedAccessException(errorMessage);
     }
